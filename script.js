a53e8ae189cfecbdc93af023491728de36b9facc
DustinYansberg <DustinYansberg@gmail.com>
2024-01-18 11:07:48 -0800

    added a carousel that is working. Need to update it with my actual projects.

--- script.js
+++ script.js
@@ -53,4 +53,23 @@ const scrollToId = (elemId) => document.getElementById(elemId).scrollIntoView();
 
 // CAROUSEL STUFF
 
-const track = document.querySelector(".carousel__track");
+$(document).ready(function () {
+  $("#myCarousel").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $("#myCarousel .carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item").eq(i).appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item").eq(0).appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
